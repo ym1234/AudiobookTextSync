f50a7f70649dd44bb18d26a1105a2947200e3df4
ym555 <mohamed.yoyo100@gmail.com>
2024-08-04 15:05:46 +0300

    get all tracebacks because debugging this is a nightmare

--- ats/extra/semiglobal.py
+++ ats/extra/semiglobal.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pprint import pprint
 
-def traceback(x, y, H, E, F, cx, cy, match, mismatch, gap_open, gap_extend):
+def traceback(x, y, H, E, F, cx, cy, match, mismatch, gap_open, gap_extend, start=False):
     cur, traceback = 0, []
     while cx > 0 and cy > 0:
         if cur == 0:
@@ -23,9 +23,17 @@ def traceback(x, y, H, E, F, cx, cy, match, mismatch, gap_open, gap_extend):
             if (H[cx-1, cy] + gap_open) == F[cx, cy]:
                 cur = 0
             cx -= 1
-    return np.array(traceback)[::-1].swapaxes(-1, -2)-1
-    # return traceback
 
+    if start:
+        while cx > 0:
+            traceback.append((cx, cy))
+            cx -= 1
+
+        while cy > 0:
+            traceback.append((cx, cy))
+            cy -= 1
+
+    return np.array(traceback)[::-1].swapaxes(-1, -2)-1
 
 def semiglobal(x, y, match=-1, mismatch=-1, gap_open=-1, gap_extend=-1, recursive=False, end=False):
     lx, ly = len(x), len(y)
@@ -33,10 +41,6 @@ def semiglobal(x, y, match=-1, mismatch=-1, gap_open=-1, gap_extend=-1, recursiv
     h = np.zeros((lx+1, ly+1))
     e = np.full((lx+1, ly+1), fill_value=-np.inf)
     f = np.full((lx+1, ly+1), fill_value=-np.inf)
-
-    Th = np.zeros((lx+1, ly+1), dtype=np.int8)
-    Te = np.zeros((lx+1, ly+1), dtype=np.int8)
-    Tf = np.zeros((lx+1, ly+1), dtype=np.int8)
 
     # Gap extends being MORE than than gap opens doesn't make much sense
     for i in range(1, ly+1):
@@ -47,31 +51,11 @@ def semiglobal(x, y, match=-1, mismatch=-1, gap_open=-1, gap_extend=-1, recursiv
         for j in range(1, ly+1):
             score = match if x[i-1] == y[j-1] else mismatch
             e[i, j] = max(e[i, j-1]+gap_extend, h[i, j-1]+gap_open)
-            Te[i, j] = 0 if e[i, j] == (e[i, j-1]+gap_extend) else 2
             f[i, j] = max(f[i-1, j]+gap_extend, h[i-1, j]+gap_open)
-            Tf[i, j] = 1 if f[i, j] == (f[i-1, j]+gap_extend) else 2
             h[i, j] = max(e[i, j], f[i, j], h[i-1, j-1]+score)
-            Th[i, j] = 0 if h[i, j] == e[i, j] else 1 if h[i, j] == f[i, j] else 2
 
-    cx, cy = lx if end else h[:, -1].T.argmax(), ly
-    return traceback(x, y, h, e, f, cx, cy, match, mismatch, gap_open, gap_extend)
-    # cur = Th[cx, cy]
-    # traceback = []
-    # while cx > 0 and cy > 0:
-    #     if cur == 2:
-    #         cur = Th[cx, cy]
-    #         if cur != 2:
-    #             continue
-    #         dx, dy = -1, -1
-    #     elif cur == 1:
-    #         cur = Tf[cx, cy]
-    #         dx, dy = -1, 0
-    #     else:
-    #         cur = Te[cx, cy]
-    #         dx, dy = 0, -1
-    #     traceback.append((cx-1, cy-1))
-    #     cx, cy = cx+dx, cy+dy
-    # return np.array(traceback)[::-1].swapaxes(-1, -2)
+    cx = lx if end else h[:, -1].T.argmax()
+    return traceback(x, y, h, e, f, cx, ly, match, mismatch, gap_open, gap_extend)
 
 def tracep(x, y, trace):
     x, y = np.array(list(x)), np.array(list(y))
@@ -130,11 +114,6 @@ def nw(x, y, match=1, mismatch=-1, gap_open=-1, gap_extend=-1):
     e = np.full((lx+1, ly+1), fill_value=-np.inf)
     f = np.full((lx+1, ly+1), fill_value=-np.inf)
 
-
-    Th = np.zeros((lx+1, ly+1), dtype=np.int8)
-    Te = np.zeros((lx+1, ly+1), dtype=np.int8)
-    Tf = np.zeros((lx+1, ly+1), dtype=np.int8)
-
     for i in range(1, ly+1):
         e[0, i] = max(e[0, i-1]+gap_extend, h[0, i-1]+gap_open)
         h[0, i] = e[0, i]
@@ -147,39 +126,13 @@ def nw(x, y, match=1, mismatch=-1, gap_open=-1, gap_extend=-1):
         for j in range(1, ly+1):
             score = match if x[i-1] == y[j-1] else mismatch
             e[i, j] = max(e[i, j-1]+gap_extend, h[i, j-1]+gap_open)
-            Te[i, j] = 0 if e[i, j] == (e[i, j-1]+gap_extend) else 2
             f[i, j] = max(f[i-1, j]+gap_extend, h[i-1, j]+gap_open)
-            Tf[i, j] = 1 if f[i, j] == (f[i-1, j]+gap_extend) else 2
             h[i, j] = max(e[i, j], f[i, j], h[i-1, j-1]+score)
-            Th[i, j] = 0 if h[i, j] == e[i, j] else 1 if h[i, j] == f[i, j] else 2
 
-    cx, cy = lx, ly
-    cur = Th[cx, cy]
-    traceback = []
-    while cx > 0 and cy > 0:
-        if cur == 2:
-            cur = Th[cx, cy]
-            if cur != 2:
-                continue
-            dx, dy = -1, -1
-        elif cur == 1:
-            cur = Tf[cx, cy]
-            dx, dy = -1, 0
-        else:
-            cur = Te[cx, cy]
-            dx, dy = 0, -1
-        traceback.append((cx-1, cy-1))
-        cx, cy = cx+dx, cy+dy
-
-    return np.array(traceback)[::-1].swapaxes(-1, -2)
+    return traceback(x, y, h, e, f, lx, ly, match, mismatch, gap_open, gap_extend, start=True)
 
 def hirschberg_inner(x, y, match, mismatch, gap_open, gap_extend):
     lx, ly = len(x), len(y)
-    if lx == 0:
-        return np.array([[0]*ly, range(ly)])
-    if ly == 0:
-        return np.array([range(lx), [0]*lx])
-
     if lx < 3 or ly < 3:
         return nw(x, y, match, mismatch, gap_open, gap_extend)
 
@@ -188,8 +141,8 @@ def hirschberg_inner(x, y, match, mismatch, gap_open, gap_extend):
 
     j =  f + s[::-1]
     k =  fe + se[::-1] - gap_open
-    mid, mid2 = j.argmax(), k.argmax()
-    print(mid, mid2)
+    # mid, mid2 = len(j) - j[::-1].argmax() - 1, len(k) - k[::-1].argmax() - 1
+    mid, mid2 = j.argmax(), len(k) - k[::-1].argmax() - 1
 
     if mid == mid2 or j[mid] >= k[mid2]:
         split1 = hirschberg_inner(x[:mid], y[:ly//2], match, mismatch, gap_open, gap_extend)
@@ -199,7 +152,7 @@ def hirschberg_inner(x, y, match, mismatch, gap_open, gap_extend):
         print("HERE", mid2, k[mid2], mid, j[mid])
         split1 = hirschberg_inner(x[:mid2], y[:ly//2-1], match, mismatch, gap_open, gap_extend)
         split2 = hirschberg_inner(x[mid2:], y[ly//2+1:], match, mismatch, gap_open, gap_extend)
-        return np.concatenate([split1, np.array([[-np.inf], [-np.inf]]), np.array([[mid2], [ly//2+1]]) + split2], axis=1)
+        return np.concatenate([split1, np.array([[mid2], [ly//2-1]]), np.array([[mid2], [ly//2]]), np.array([[mid2], [ly//2+1]]) + split2], axis=1)
 
 
 def pyhirschberg(x, y, match=1, mismatch=-1, gap_open=-1, gap_extend=-1, end=False):
