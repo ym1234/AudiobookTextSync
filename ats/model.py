8d19e988ce567b0e4425496761cdd201f77a5cab
ym555 <mohamed.yoyo100@gmail.com>
2024-10-21 20:47:49 +0300

    ugh

--- ats/model.py
+++ ats/model.py
@@ -147,7 +147,8 @@ class Tokenizer:
 class Model:
     def __init__(self, model_size_or_path, device='auto', device_index=None, quantize=True, download_root=None, local_files_only=False):
         model_path = model_size_or_path if os.path.isdir(model_size_or_path) else download_model(model_size_or_path, download_root, local_files_only)
-        device = 'cpu' if get_cuda_device_count() == 0 else device
+        num_cuda = get_cuda_device_count()
+        device = 'cpu' if  num_cuda == 0 else device
         device_index = device_index if device_index is not None else list(range(num_cuda)) if device == 'cuda' else 0
         self.model = Whisper(model_path, device=device, device_index=device_index, compute_type='auto' if quantize else 'default')
                              # intra_threads=multiprocessing.cpu_count()) # I have no idea why this makes it **slower**
