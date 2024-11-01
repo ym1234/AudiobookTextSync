10457d57614dada2e5e1f4f771ec619e889351fe
ym555 <mohamed.yoyo100@gmail.com>
2024-11-01 05:24:47 +0200

    typo

--- ats/audio.py
+++ ats/audio.py
@@ -130,7 +130,7 @@ class MelProcess:
         yield self.mel(buffer[:nread+leftover], lmax)[0][:, :-1]
 
     def gpu_mel(self, buffer, lmax):
-        stft = signal.stft(cp.asarray(buffer), fs=SAMPLE_RATE, window='hann', nperseg=N_FFT, noverlap=N_FFT-HOP_LENGTH, nfft=N_FFT, return_onesided=False)[0]
+        stft = signal.stft(cp.asarray(buffer), fs=SAMPLE_RATE, window='hann', nperseg=N_FFT, noverlap=N_FFT-HOP_LENGTH, nfft=N_FFT, return_onesided=False)[-1]
         stft = stft[:(N_FFT >> 1) + 1 , -1]
         magnitudes = cp.abs(stft) ** 2
 
