2efd2be81a3c7f351730a718c42e09ee3d356464
ym555 <mohamed.yoyo100@gmail.com>
2024-11-01 04:03:35 +0200

    aaaa

--- ats/__main__.py
+++ ats/__main__.py
@@ -206,7 +206,7 @@ def whisper(audio, text, language, output_dir, output_format, file_overwrite,
             bar.set_description(audio[ai].path.name)
             segments = []
             for astart, aend, book, tstart, tend, _ in tqdm(batches):
-                language = get_lang(transcription_grouped[ai][astart]].language, prepend, append, nopend)
+                language = get_lang(transcription_grouped[ai][astart].language, prepend, append, nopend)
                 tcontent = [s for i in range(tstart, tend) for s in text[book].chapters[i].text()]
                 acontent = [s for i in range(astart, aend) for s in transcription_grouped[ai][i].segments]
                 alignment, references = align.align(aligner, language, acontent, tcontent, [], set(), set(), set())
